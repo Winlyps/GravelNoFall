5a4d2612087a703289d0d21b60fb17157637a13c
Winlypse <nts3@o2.pl>
2024-10-18 13:11:54 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/gravelNoFall/GravelListener.kt
@@ -0,0 +1,4 @@
+package winlyps.gravelNoFall
+
+class GravelListener {
+}
---
+++ src/main/kotlin/winlyps/gravelNoFall/GravelNoFall.kt
@@ -0,0 +1,14 @@
+package winlyps.gravelNoFall
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GravelNoFall : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
